7417144658e77476eed2314f2ddbd15bd6c0b297
Jeremy Fuksa <hello@orangefla.me>
2026-01-22 10:16:31 -0600

    Restore Select import that was incorrectly removed
    - Add back Select, SelectContent, SelectItem, SelectTrigger, SelectValue imports
    - These components are still used in DeviceTab for dropdowns throughout the file

--- frontend/src/app/components/views/DeviceTab.tsx
+++ frontend/src/app/components/views/DeviceTab.tsx
@@ -20,6 +20,13 @@ import { useAPI } from "../../../api/useApi";
 import { useStore } from "../../../store/useStore";
 import { Slider } from "../ui/slider";
 import { Switch } from "../ui/switch";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 type DeviceCategory =
   | "Sync"
